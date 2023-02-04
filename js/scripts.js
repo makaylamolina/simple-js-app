fb8adaf301f5a4dd98c751233bba49165abf8edc
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-02-04 16:55:26 -0500

    transfer to github

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let myName = 'Makayla';
+document.write(myName);
+myName = 'Molina';
+document.write(myName);
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(doubleSize);
