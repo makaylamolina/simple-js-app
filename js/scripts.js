c3aaa74215f3f3a23933a2d8a4c6600b9f73a8bc
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-02-11 14:11:11 -0500

    added first three pokemon

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    { name: 'Charmander', height: 0.6, type: 'fire'}
+    { name: 'Charmeleon', height: 1.1, type: 'fire'}
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
+]
