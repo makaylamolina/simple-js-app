54b3b1ec4abf729f9a05aefd9d67dd8cd9ebd003
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-02-14 10:38:27 -0500

    added for loop to write out each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     { name: 'Charmeleon', height: 1.1, type: 'fire'},
     { name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
 ]
+
+//loop to write out each pokemon
+for (let i = 0; i <=pokemonList.length; i++) {
+  if (pokemonList[i].height >= 1.5) {
+    document.write(pokemonList[i].name + ' (height : ' + pokemonList[i].height + ') Wow, that\'s big! ');
+    } else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) This one isn\'t very big. ');
+  }
+}
