6ec9a9f8676c97f45a38722b270bec59dafb1869
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-02-22 21:39:10 -0500

    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-let pokemonList = [
-    { name: 'Charmander', height: 0.6, type: 'fire'},
-    { name: 'Charmeleon', height: 1.1, type: 'fire'},
-    { name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
-];
-let pokemonList2 = [
-    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    { name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
-    { name: 'Venusaur', height: 2, type: ['grass', 'poison']}
-  ];
+let pokemonRepository = (function () {
+    let pokemonList = [
+      { name: 'Charmander', height: 0.6, type: ['fire']},
+      { name: 'Charmeleon', height: 1.1, type: ['fire']},
+      { name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
+    ];
+  
+    let pokemonList2 = [
+      { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+      { name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+      { name: 'Venusaur', height: 2, type: ['grass', 'poison']}
+    ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
 
 //loop to write out each pokemon
 function printArrayDetails(pokemon) {
@@ -20,5 +36,4 @@ function printArrayDetails(pokemon) {
     }
   }
   
-  pokemonList.forEach(printArrayDetails);
-  pokemonList2.forEach(printArrayDetails);
+  pokemonRepository.getAll().forEach(printArrayDetails);
