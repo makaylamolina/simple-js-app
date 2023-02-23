499ac2844959c207eaec3f5453ac363f180a8abf
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-02-22 21:24:48 -0500

    add pokemonList2

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ let pokemonList = [
     { name: 'Charmander', height: 0.6, type: 'fire'},
     { name: 'Charmeleon', height: 1.1, type: 'fire'},
     { name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
-]
+];
+let pokemonList2 = [
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    { name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+    { name: 'Venusaur', height: 2, type: ['grass', 'poison']}
+  ];
 
 //loop to write out each pokemon
 for (let i = 0; i <=pokemonList.length; i++) {
